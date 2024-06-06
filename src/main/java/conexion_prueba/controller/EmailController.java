05789f41725cff77fa8900bfae99a41f0ea067b7
kevinarauz <kecin222294@gmail.com>
2024-06-05 22:17:34 -0500

    Se agrega response

--- src/main/java/conexion_prueba/controller/EmailController.java
+++ src/main/java/conexion_prueba/controller/EmailController.java
@@ -1,6 +1,11 @@
 package conexion_prueba.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +22,12 @@ public class EmailController {
     private EmailService emailService;
 	
 	@GetMapping("/send-email")
-    public String sendEmail() {
+    public ResponseEntity<?> sendEmail() {
         //emailService.sendSimpleMessage("destinatario@gmail.com", "Asunto del Correo", "Contenido del correo");
 		emailService.sendSimpleMessage("kevin.arauzg@gmail.com", "Prueba de Correo", "Esto es una prueba :).");
-        return "Correo enviado!";
+        Map<String, String> respuesta = new HashMap<>();
+        respuesta.put("mensaje", "Correo enviado!");
+        return new ResponseEntity<>(respuesta, HttpStatus.OK);
     }
 	
 }
